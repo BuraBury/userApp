4bfe8eb127971540c204e83aa1e21a928eed16dd
burabury <bura.bury@gmail.com>
2020-10-14 14:26:46 +0200

    update

---
+++ src/app/component/lamborgini/lamborgini.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lamborgini',
+  templateUrl: './lamborgini.component.html',
+  styleUrls: ['./lamborgini.component.css']
+})
+export class LamborginiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
